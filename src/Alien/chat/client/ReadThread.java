023d430c495f5b3b9f834feac0cb3853608cc5a1
Radimir Karmatskiy <rkarmatskiy@phoenixit.ru>
2021-07-02 18:12:40 +0300

    Код взят отсюда: https://www.codejava.net/java-se/networking/how-to-create-a-chat-console-application-in-java-using-socket

---
+++ src/Alien/chat/client/ReadThread.java
@@ -0,0 +1,51 @@
+package Alien.chat.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+/**
+ * This thread is responsible for reading server's input and printing it
+ * to the console.
+ * It runs in an infinite loop until the client disconnects from the server.
+ *
+ * @author www.codejava.net
+ */
+public class ReadThread extends Thread {
+    private final Socket socket;
+    private final ChatClient client;
+    private BufferedReader reader;
+
+    public ReadThread(Socket socket, ChatClient client) {
+        this.socket = socket;
+        this.client = client;
+
+        try {
+            InputStream input = socket.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(input));
+        } catch (IOException ex) {
+            System.out.println("Error getting input stream: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                String response = reader.readLine();
+                System.out.println("\n" + response);
+
+                // prints the username after displaying the server's message
+                if (client.getUserName() != null) {
+                    System.out.print("[" + client.getUserName() + "]: ");
+                }
+            } catch (IOException ex) {
+                System.out.println("Error reading from server: " + ex.getMessage());
+                ex.printStackTrace();
+                break;
+            }
+        }
+    }
+}
